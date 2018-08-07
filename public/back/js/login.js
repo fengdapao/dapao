660b2a06d3260378d75411e2442d2b15353a83b5
fengdapao <907891399@qq.com>
2018-08-07 12:53:44 +0800

    初始化

---
+++ public/back/js/login.js
@@ -0,0 +1,52 @@
+$(function(){
+  
+  $('#form').bootstrapValidator({
+    // 图标
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    },
+    fields:{
+  
+      username: {
+        validators: {
+          //不能为空
+          notEmpty: {
+            message: '用户名不能为空'
+          },
+          //长度校验
+          stringLength: {
+            min: 2,
+            max: 6,
+            message: '用户名长度必须在2到6之间'
+          },
+        }
+      },
+
+      password: {
+        validators: {
+          //不能为空
+          notEmpty: {
+            message: '密码不能为空'
+          },
+          //长度校验
+          stringLength: {
+            min: 6,
+            max: 12,
+            message: '密码长度必须在6到12之间'
+          },
+        }
+      } 
+
+    }
+
+  })
+
+
+
+
+
+
+
+})
